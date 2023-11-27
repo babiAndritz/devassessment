7df2dcff7e1e182b61664cfccbf71fb2eb244d44
Barbara Luciano <barbara.araujo@andritz.com>
2023-11-27 15:45:07 -0300

    using 2x1; read no proprio while com nova condição de parada

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -15,59 +15,45 @@ namespace SubtitleTimeshift
             var i = 0;
 
             using (StreamReader streamReader = new StreamReader(input, encoding))
-            {
-                while (count < 2)
-                {
-                    string line = await streamReader.ReadLineAsync();
-
-                    if (line != null)
-                    {
-                        if (line.Contains("Subtitle by : Amir Barzagli"))
-                        {
-                            count++;
-                        }
-
-                        if (line.Contains("-->"))
-                        {
-
-                            var subs = line.Substring(0, 12);
-                            var time = TimeSpan.Parse(subs);
-
-                            time += timeSpan;
-                            var formatedTime = time.ToString(@"hh\:mm\:ss\.fff");
-                            line = line.Replace(subs, formatedTime);
-
-                            subs = line.Substring(line.LastIndexOf('>') + 2);
-                            time = TimeSpan.Parse(subs);
-
-                            time += timeSpan;
-                            formatedTime = time.ToString(@"hh\:mm\:ss\.fff");
-                            line = line.Replace(subs, formatedTime);
-
-                        }
-
-                        lines[i] = line;
-
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-                    i++;
-                }
-            }
-
             using (StreamWriter streamWriter = new StreamWriter(output, encoding))
             {
-                foreach (string line in lines)
+
+                var line = default(string);
+
+                while (null != (line = await streamReader.ReadLineAsync()))
                 {
-                    if (line != null)
+                    if (line.Contains("-->"))
                     {
-                        await streamWriter.WriteLineAsync(line);
+
+                        var subs = line.Substring(0, 12);
+                        var time = TimeSpan.Parse(subs);
+
+                        time += timeSpan;
+                        var formatedTime = time.ToString(@"hh\:mm\:ss\.fff");
+                        line = line.Replace(subs, formatedTime);
+
+                        subs = line.Substring(line.LastIndexOf('>') + 2);
+                        time = TimeSpan.Parse(subs);
+
+                        time += timeSpan;
+                        formatedTime = time.ToString(@"hh\:mm\:ss\.fff");
+                        line = line.Replace(subs, formatedTime);
+
+                    }
+
+                    lines[i] = line;
+                    i++;
+                }
+
+                foreach (string l in lines)
+                {
+                    if (l != null)
+                    {
+                        await streamWriter.WriteLineAsync(l);
                     }
                 }
             }
+
         }
     }
 }
