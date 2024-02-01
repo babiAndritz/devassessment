6e2f91f4c26f98394780eb5241ce08b4261a5514
Barbara Luciano <barbara.araujo@andritz.com>
2024-02-01 14:21:05 -0300

    All click functions passed into commands in the VM file

--- Challenge/View/SubtitleWindow.xaml.cs
+++ Challenge/View/SubtitleWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Win32;
 using System.Windows.Controls;
+using MahApps.Metro.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -32,64 +33,6 @@ namespace Challenge.View
             InitializeComponent();
             viewModel = new SubtitleVM();
             DataContext = viewModel;
-        }
-
-        private void TimeSpanButton_Click(object sender, RoutedEventArgs e)
-        {
-
-            
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Subtitle files (*.srt)|*.srt|All files(*.*)|*.*";
-            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            if (dialog.ShowDialog() == true)
-            {
-                viewModel.InputFile = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read);
-
-                MessageBox.Show($"File selected with success: {dialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-
-                string fileName = Path.GetFileName(dialog.FileName);
-
-                FileResult.Text = $"File: {fileName}";
-            }
-        }
-
-        private async void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (viewModel.InputFile == null)
-            {
-                MessageBox.Show("Please select a file, then click this button.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
-            } else if (viewModel.TimeSpan <= TimeSpan.Zero)
-            {
-                MessageBox.Show("Please write the timeSpan you wish to use on the subtitle, then click this button.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            else
-            {
-                using (viewModel.InputFile)
-                {
-                    SaveFileDialog saveFileDialog = new SaveFileDialog();
-                    saveFileDialog.Filter = "Subtitle files (*.srt)|*.srt|All files(*.*)|*.*";
-                    saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                    if (saveFileDialog.ShowDialog() == true)
-                    {
-                        viewModel.OutputFile = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
-                        await Shifter.Shift(viewModel.InputFile, viewModel.OutputFile, viewModel.TimeSpan, viewModel.Encoding);
-
-                        MessageBox.Show($"File modified and saved with success: {saveFileDialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-
-                        string fileName = Path.GetFileName(saveFileDialog.FileName);
-
-                        OutFileResult.Text = $"Output File: {fileName}";
-
-                    }
-                }
-            }
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
--- Challenge/ViewModel/SubtitleVM.cs
+++ Challenge/ViewModel/SubtitleVM.cs
@@ -3,16 +3,17 @@ using System;
 using System.ComponentModel;
 using System.IO;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
+using Microsoft.Win32;
+using SubtitleTimeshift;
 
 namespace Challenge.ViewModel
 {
     public class SubtitleVM : INotifyPropertyChanged
     {
-        
-
         private Stream inputFile;
-        public Stream InputFile
+        public Stream? InputFile
         {
             get { return inputFile; }
             set
@@ -23,7 +24,7 @@ namespace Challenge.ViewModel
         }
 
         private Stream outputFile;
-        public Stream OutputFile
+        public Stream? OutputFile
         {
             get { return outputFile; }
             set
@@ -55,24 +56,140 @@ namespace Challenge.ViewModel
             }
         }
 
+        public ICommand SaveCommand { get; set; }
+        public ICommand SelectFileCommand { get; set; }
+        public ICommand OutputFileCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+
+        public string timeResultText;
+        public string TimeResultText
+        {
+            get { return timeResultText; }
+            set
+            {
+                timeResultText = value;
+                OnPropertyChanged("TimeResultText");
+            }
+        }
+
+        public string timeText;
+        public string TimeText
+        {
+            get { return timeText; }
+            set
+            {
+                timeText = value;
+                OnPropertyChanged("TimeText");
+            }
+        }
+
+        public string fileResultText;
+        public string FileResultText
+        {
+            get { return fileResultText; }
+            set
+            {
+                fileResultText = value;
+                OnPropertyChanged("FileResultText");
+            }
+        }
+
+        public string outFileResultText;
+        public string OutFileResultText
+        {
+            get { return outFileResultText; }
+            set
+            {
+                outFileResultText = value;
+                OnPropertyChanged("OutFileResultText");
+            }
+        }
+
+        public SubtitleVM()
+        {
+
+            SaveCommand = new DelegateCommand(() =>
+            {
+                if (long.TryParse(TimeText, out long milliseconds))
+                {
+                    TimeSpan = TimeSpan.FromMilliseconds(milliseconds);
+                    TimeText = string.Empty;
+                    TimeResultText = TimeSpan.ToString();  
+                }
+                else
+                {
+                    MessageBox.Show("Please write a correct TimeSpan.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            });
+
+            SelectFileCommand = new DelegateCommand(() =>
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Subtitle files (*.srt)|*.srt|All files(*.*)|*.*";
+                dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                if (dialog.ShowDialog() == true)
+                {
+                    InputFile = new FileStream(dialog.FileName, FileMode.Open, FileAccess.Read);
+
+                    MessageBox.Show($"File selected with success: {dialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    string fileName = Path.GetFileName(dialog.FileName);
+
+                    FileResultText = $"File: {fileName}";
+                }
+
+            });
+
+            OutputFileCommand = new DelegateCommand(async () =>
+            {
+                if (InputFile == null)
+            {
+                    MessageBox.Show("Please select a file, then click this button.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else if (TimeSpan <= TimeSpan.Zero)
+                {
+                    MessageBox.Show("Please write the timeSpan you wish to use on the subtitle, then click this button.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    using (InputFile)
+                    {
+                        SaveFileDialog saveFileDialog = new SaveFileDialog();
+                        saveFileDialog.Filter = "Subtitle files (*.srt)|*.srt|All files(*.*)|*.*";
+                        saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                        if (saveFileDialog.ShowDialog() == true)
+                        {
+                            OutputFile = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
+                            await Shifter.Shift(InputFile, OutputFile, TimeSpan, Encoding);
+
+                            MessageBox.Show($"File modified and saved with success: {saveFileDialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                            string fileName = Path.GetFileName(saveFileDialog.FileName);
+
+                            OutFileResultText = $"Output File: {fileName}";
+
+                        }
+                    }
+                }
+            });
+
+            ClearCommand = new DelegateCommand(() =>
+            {
+                TimeSpan = TimeSpan.Zero;
+                TimeResultText = String.Empty;
+                InputFile = null;
+                FileResultText = String.Empty;
+                OutputFile = null;
+                OutFileResultText = String.Empty;
+            });
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public ICommand SaveCommand { get; set; }
-        public string texto { get; set; }
-
-
-        public SubtitleVM()
-        {
-            timeSpan = new TimeSpan(123);
-            SaveCommand = new DelegateCommand(() =>
-            {
-                texto = timeSpan.ToString();
-                
-            });
         }
     }
 }
